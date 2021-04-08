ceda31c3452d6efecde1aab759fb904bed6204c5
Kevin Boosten <kboosten@ilionx.com>
2021-04-08 22:46:21 +0200

    feat: add staggered animation example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,55 @@
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        // each time the binding value changes
+        query(
+          ':leave',
+          [stagger(100, [animate('0.5s', style({ opacity: 0 }))])],
+          { optional: true }
+        ),
+        query(
+          ':enter',
+          [
+            style({ opacity: 0 }),
+            stagger(100, [animate('0.5s', style({ opacity: 1 }))]),
+          ],
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
 export class HomePage {
+  items = [];
 
-  constructor() {}
+  public showItems() {
+    this.items = Array(15)
+      .fill(null)
+      .map((a, index) => ({
+        id: index,
+        title: `some title ${index}`,
+        image: `https://i.pravatar.cc/40?img=${index}`,
+      }));
+  }
 
+  public hideItems() {
+    this.items = [];
+  }
+
+  public toggle() {
+    this.items.length ? this.hideItems() : this.showItems();
+  }
 }
